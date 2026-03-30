921908c86635174e72143e6e925373b210a126d5
Ubuntu <ubuntu@ip-172-31-102-159.ap-southeast-2.compute.internal>
2026-03-30 12:57:00 +0000

    Update

---
+++ frontend/src/axoisConfig.jsx
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://13.54.26.5:5001', // live
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default axiosInstance;
